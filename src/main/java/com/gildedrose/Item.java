d84faf7ffdd8e851168e92b5137c1f5e0d84e878
Data-MaSTeRR <hyunwoo8504@gmail.com>
2026-04-01 11:30:08 +0900

    Feat: Item 캡슐화

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -2,20 +2,30 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
+    private ItemType name;
+    private int sellIn;
+    private int quality;
 
-    public int sellIn;
-
-    public int quality;
-
-    public Item(String name, int sellIn, int quality) {
+    public Item(ItemType name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    public ItemType getName() {
+        return this.name;
+    }
+
+    public int getSellIn() {
+        return this.sellIn;
+    }
+
+    public int getQuality() {
+        return this.quality;
+    }
+
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
